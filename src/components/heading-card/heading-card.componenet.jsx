2dbe702d489a951ca91a1772a3e5efd3f09a5783
1shtandon <202052344@iiitvadodara.ac.in>
2023-04-12 02:54:13 +0530

    UI fixes in submission details

--- src/components/heading-card/heading-card.componenet.jsx
+++ src/components/heading-card/heading-card.componenet.jsx
@@ -4,26 +4,30 @@ import "./heading-card.styles.scss";
 
 const HeadingCard = () => {
     return (
-        <div className="heading-card-container">
-            <div className="heading-card-info">
-                <div className="heading-card-title">
-                    Hackathon Submissions
+        <div className="parent-container">
+            <div className="heading-card-container">
+                <div className="heading-card-info">
+                    <div className="heading-card-title">
+                        Hackathon Submissions
+                    </div>
+                    <p className="heading-card-description">
+                        Lorem ipsum dolor sit amet consectetur. Urna cursus amet pellentesque in parturient purus feugiat faucibus. Congue laoreet duis porta turpis eget suspendisse ac pharetra amet. Vel nisl tempus nec vitae.
+                    </p>
+                    <Link to="/upload" className="upload-link">
+                        <button className="upload-btn">
+                            Upload Submission
+                        </button>
+                    </Link>
                 </div>
-                <p className="heading-card-description">
-                    Lorem ipsum dolor sit amet consectetur. Urna cursus amet pellentesque in parturient purus feugiat faucibus. Congue laoreet duis porta turpis eget suspendisse ac pharetra amet. Vel nisl tempus nec vitae.
-                </p>
-                <Link to="/upload" className="upload-link">
-                    <button className="upload-btn">
-                        Upload Submission
-                    </button>
-                </Link>
-            </div>
-            <div className="heading-card-image">
-                <Bulb className="bulb" />
+                <div className="heading-card-image">
+                    <Bulb className="bulb" />
+
+                </div>
+                <div className='waves' />
 
             </div>
-
         </div>
+
     );
 };
 
